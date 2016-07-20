f76a37a2102150ce052021ce8a3a0680ea47e80d
Lustralisk <wzlsc_lustre@163.com>
2016-07-20 23:50:43 +0800

    finish model

--- Hangout/app/models.py
+++ Hangout/app/models.py
@@ -11,6 +11,29 @@ class User(Document):
     score = FloatField(default=0.0)
     portrait = ImageField()
     email = URLField()
+    org_acts = ListField(IntField())
+    join_acts = ListField(IntField())
+    coll_acts = ListField(IntField())
+    fix_times = ListField(IntField())
+    tmp_times = ListField(IntField())
+    tags = ListField(StringField(max_length=100))
+    friends = ListField(IntField())
+    messages = ListField(DictField())
+    # from: IntField(), content: StringField()
+
+class Activity(Document):
+    name = StringField(required=True, max_length=100)
+    intro = StringField(required=True)
+    tags = ListField(required=True, StringField(max_length=100))
+    cost = FloatField(default=0.0)
+    organizer = IntField(required=True)
+    admins = ListField(IntField())
+    applicants = ListField(IntField())
+    members = ListField(IntField())
+    notices = ListField(DictField())
+    # from: IntField(), content: StringField()
+    messages = ListField(DictField())
+    # from: IntField(), content: StringField()
     
 
 # no need for site.admin.register
